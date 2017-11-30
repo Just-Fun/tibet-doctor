f473c982ba6666d57b7069e71adbcc1f13df9285
Sergii Zagryvyi <serzhcello@gmail.com>
2017-11-30 21:31:50 +0200

    start project

---
+++ src/main/java/com/serzh/tibetdoctor/TibetDoctorApplication.java
@@ -0,0 +1,12 @@
+package com.serzh.tibetdoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TibetDoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TibetDoctorApplication.class, args);
+	}
+}
